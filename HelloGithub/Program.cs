481ec5d6fcfaf692c1a822cd6d8e048819c6d0fe
AlbertOsinaga <luisalberto.osinaga@gmail.com>
2019-03-15 12:14:28 -0400

    Modificado a Hello Github

--- HelloGithub/Program.cs
+++ HelloGithub/Program.cs
@@ -6,7 +6,7 @@ namespace HelloGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Github!");
         }
     }
 }
